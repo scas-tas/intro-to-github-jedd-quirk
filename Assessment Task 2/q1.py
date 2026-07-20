a9a1275cd1aaafbe9aa50bf5950afca5ed1ffa79
jedd-quirk <jedd-quirk@scas.nsw.edu.au>
2026-07-20 10:29:06 +1000

    Update q1.py

---
+++ Assessment Task 2/q1.py
@@ -0,0 +1,7 @@
+def combine_trucks(t, t1, t2):
+    return t[t1]+t[t2]
+def main():
+    t=[0,4,7,2,6,9]
+    print(combine_trucks(t, 2, 4))  # Expected: 13
+    print(combine_trucks(t, 1, 3))  # Expected: 6
+main()
