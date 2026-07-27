2f25503e52c1e6d6e501214b0f2ad9e06b1582c7
jedd-quirk <jedd-quirk@scas.nsw.edu.au>
2026-07-28 09:09:12 +1000

    create python for q2

---
+++ Assessment Task 2/q2.py
@@ -0,0 +1,19 @@
+def count_empty(classroom: list) -> int:
+    count = 0
+    for row in classroom:
+        for i in row:
+            if i==0: count+=1
+    return count
+ 
+def most_empty_row(classroom: list) -> int:
+    best_row = 0
+    best_count = -1
+    for row in classroom:
+        if row.count(0)>best_count:
+            best_count=row.count(0)
+            best_row=classroom.index(row)
+    return best_row
+
+classroom = [[2],[0,1],[2,0,0],[4,2,3]]
+print(most_empty_row(classroom))
+print(count_empty(classroom))
