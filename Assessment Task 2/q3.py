461ffeddaa24f609b6a3046ed6f45cbcfc6f2c81
jedd-quirk <jedd-quirk@scas.nsw.edu.au>
2026-07-28 13:22:07 +1000

    removed pseudoq3 and completed q3

--- Assessment Task 2/q3.py
+++ Assessment Task 2/q3.py
@@ -1,8 +1,30 @@
 def add_item(inventory: dict, name: str, quantity: int) -> None:
-    pass
+    if name in inventory: inventory[name]+=quantity
+    else: inventory[name]=quantity
  
 def remove_item(inventory: dict, name: str, quantity: int) -> None:
-    pass
+    if name in inventory:
+        if quantity>=inventory[name]:
+            inventory[name]=0
+        else:
+            inventory[name]-=quantity
+    else:
+        inventory[name]=-1
  
 def get_stock_report(inventory: dict) -> str:
-    pass
+    for i in inventory:
+        if inventory[i]>0:
+            print(f"{i}: {inventory[i]}")
+        else:
+            if inventory[i]!=-1: print(f"({i} at 0 -- excluded)")
+            else: print(f"(no error -- {i} never existed)")
+
+inv={}
+add_item(inv, 'apples', 10)
+add_item(inv, 'bananas', 5)
+get_stock_report(inv)
+add_item(inv, 'apples', 5)
+remove_item(inv, 'bananas', 10)
+get_stock_report(inv)
+remove_item(inv, 'oranges', 3)
+get_stock_report(inv)
