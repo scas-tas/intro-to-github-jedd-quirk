3c00f6162569a65fce058f903fb64b23a4c4e6b8
jedd-quirk <jedd-quirk@scas.nsw.edu.au>
2026-08-10 11:35:28 +1000

    Update q4.py

--- Assessment Task 2/q4.py
+++ Assessment Task 2/q4.py
@@ -4,4 +4,4 @@ def e(m: str, s,c="") -> str:
 def d(m: str, s,c="") -> str:
     for Mi in range(len(m)):c+=(m[Mi],chr((ord(m[Mi].lower())-97-(([s],s)[isinstance(s, str)][0],ord(str(([s],s)[isinstance(s, str)])[Mi%len(str(([s],s)[isinstance(s, str)]))])-97)[isinstance(([s],s)[isinstance(s, str)][0],str)])%26+97-32*m[Mi].isupper()))[m[Mi].isalpha()]
     return c
-print(d(e("Hello World!","dad"),"dad"))
+print(d(e("Hello World!",27),27))
